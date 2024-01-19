d3b6a3c6f0339fbce4c778b250757f573527bcbf
AshongAbdallah06 <ashongabdalla15@gmail.com>
2024-01-20 00:05:15 +0200

    Create mobile desgin

---
+++ index.js
@@ -0,0 +1,46 @@
+const width = window.innerWidth;
+
+let menubar = document.querySelector('.menu');
+
+
+function checkWidth() {
+    if (window.matchMedia('(max-width: 600px)').matches) {
+        menubar.innerHTML =
+            `<img src="assets/images/icon-menu.svg" class="menu-button">`;
+
+        document.querySelector('.web3-img-cont').innerHTML = 
+            `
+            <img class="web3-img" src="assets/images/image-web-3-mobile.jpg" alt="">
+            `
+    } else {
+        menubar.innerHTML = 
+            `
+                <p class="">Home</p>
+                <p class="">New</p>
+                <p class="">Popular</p>
+                <p class="">Trending</p>
+                <p class="">Categories</p>
+            `
+    }
+}
+checkWidth(); // Initial Load
+
+let menuBtn = document.querySelector('.menu-button');
+menuBtn.addEventListener('click', () => {
+        document.querySelector('.mmc').classList.add('show-mmc');
+        document.querySelector('.mobile-menu-cont').classList.add('mobile-menu-cont-animation')
+})
+
+
+let closeButton = document.querySelector('.close-icon');
+closeButton.addEventListener('click', () => {
+    document.querySelector('.mmc').classList.remove('show-mmc');
+    document.querySelector('.mobile-menu-cont').classList.remove('mobile-menu-cont-animation')})
+
+
+window.addEventListener('resize', () => {
+    checkWidth();
+
+
+
+})
